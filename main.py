e6e89c7856950d58aa7b308f46f29d05cbfb136c
monir <monir.bu18@gmail.com>
2024-08-25 23:47:56 +0600

    First class code done

---
+++ main.py
@@ -0,0 +1,22 @@
+from statistics import multimode
+
+print("Hello Python")
+
+x = 65
+y = 25
+
+sum = x + y
+
+sub = x - y
+
+multiply = x*y
+
+division = x / y
+
+print(sum,sub,multiply,division)
+print(sum)
+print(sub)
+print(multiply)
+print(division)
+print(x)
+print(y)
