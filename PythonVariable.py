7a0cbc5277f854d7d88b5446cac607ca29767b6c
monir <monir.bu18@gmail.com>
2024-08-26 18:49:52 +0600

    Python Variables & Comment Class-2

---
+++ PythonVariable.py
@@ -0,0 +1,24 @@
+#Python Variables -- This is Single line comment
+"""
+    - This is multiline comments
+    - Descriptive Names
+    - Lowercase with Underscores
+    - Avoid Reserved Words
+    - Start with a letter or Underscore
+    - No Special Characters
+    - Case Sensitivity
+    - Short but Meaningful
+    - Use Singular Nouns
+    - Avoid Double underscores
+"""
+num_student = 399
+print(num_student)
+
+X = 100
+x = 200
+# Summation two numbers
+y = X+x
+print(y)
+
+print(X)
+print(x)
