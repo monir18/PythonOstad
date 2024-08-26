87002dfc823768f61c8bbcedb68b3a6f79929634
monir <monir.bu18@gmail.com>
2024-08-26 23:07:07 +0600

    DataTypes Done,

---
+++ DataTypes.py
@@ -0,0 +1,31 @@
+"""
+Data Types in Python
+	- int
+	- float
+	- complex
+	- string
+	- list
+	- tuple
+	- boolean
+	- None
+	- Set Types
+	- Mapping
+"""
+
+age = 34
+taka = 1010.50
+i = 1+2j
+name = "Md. Moniruzzaman Monir"
+fruits = ["Apple","banana","cherry","Mango"]
+cities = ("Dhaka","Rajshahi","Rangpur")
+isLogin = True
+MicrophoneModel = None
+BCsQuestionSet = {"Padma","Meghna","Jamuna"}
+
+myAddress = {
+    "House":33,
+    "Road":8,
+    "PostOffice":"Adabor",
+    "Dist":"Dhaka"
+}
+
