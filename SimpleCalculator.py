a169033019161a9e9e5c213d47dee05d943cec48
monir <monir.bu18@gmail.com>
2024-08-29 01:00:47 +0600

    simple calculator

---
+++ SimpleCalculator.py
@@ -0,0 +1,18 @@
+number1 = int(input("Please input first number : "))
+number2 = int(input("Please input second number : "))
+
+sum = number1+number2
+print(sum)
+print("Summation is : ", sum)
+
+sum = number1 / number2
+print("Division is ", sum)
+
+sum = number1 - number2
+print("Abstraction is ", sum)
+
+sum = number1 * number2
+print("Multiplication is ", sum)
+
+sum = number1 % number2
+print("Modules is ",sum)
