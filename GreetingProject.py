b678ab7f1a9eb7444b3fef9360b4a0e24ad4abe0
monir <monir.bu18@gmail.com>
2024-08-29 00:43:20 +0600

    How to input int number

--- GreetingProject.py
+++ GreetingProject.py
@@ -1,2 +1,11 @@
 name = input("Please Enter Your Name: ")
 print("Hello, " + name + "!")
+
+score1 = int(input("Please input your 1st score : "))
+score2 = input("Please input your 2nd score : ")
+score2 = int(score2)
+score = score1 + score2
+print(score)
+print(score1)
+print(type(score1))
+
