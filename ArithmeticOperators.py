4c2bdfaa842ddee818ba40c23fce27cda04bc44e
monir <monir.bu18@gmail.com>
2024-09-09 00:06:14 +0600

    Arithmetic operators operation

---
+++ ArithmeticOperators.py
@@ -0,0 +1,32 @@
+val1 = 2
+val2 = 3
+
+# using the addition operator
+res = val1 + val2
+print(res)
+
+# using the subtraction operator
+res = val1 - val2
+print(res)
+
+# using the multiplication operator
+res = val1 * val2
+print(res)
+
+val3 = 3
+val4 = 2
+# using the division operator
+res = val3 / val4
+print(res)
+
+# using the floor division
+res = val3 // val4
+print(res)
+
+# using the modulus operator
+res = val3 % val4
+print(res)
+
+# using the exponentiation operator
+res = val1 ** val2
+print(res)
