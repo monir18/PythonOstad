b7fbf4cafaec6e0b2e820086c97e06a1a076a1c8
monir <monir.bu18@gmail.com>
2024-09-14 00:04:19 +0600

    list example

---
+++ python_list.py
@@ -0,0 +1,16 @@
+fruits = ["Apple","Banana","Orange","Mango"]
+print(fruits)
+print(fruits[0])
+fruits.append("Papaya")
+print(fruits)
+print(len(fruits))
+pp = "Papaya" in fruits
+print(pp)
+gv = "Guava" in fruits
+print(gv)
+
+print(fruits)
+fruits.pop()
+print(fruits)
+fruits.pop()
+print(fruits)
