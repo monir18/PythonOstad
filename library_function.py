7c8a367c49aec53a624fccb8a75f089f20f62f69
monir18 <monir.bu18@gmail.com>
2024-09-15 17:56:16 +0600

    library function example

---
+++ library_function.py
@@ -0,0 +1,15 @@
+import random
+import datetime
+import math
+
+random_number = random.randint(1,100)
+print(random_number)
+
+today = datetime.datetime.today()
+print(today)
+
+x = math.sqrt(100)
+print(x)
+
+pi = math.pi
+print(pi)
